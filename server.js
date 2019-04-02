5a3975a5eb54d6212deba8b766cd73a787267149
Alan Saber <battlecraft95@gmail.com>
2019-04-03 00:50:24 +0200

    #03 Add nodemonde and base view folder with index.html

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const express = require('express');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const path = __dirname + '/views/';
 
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello World\n');
+	res.sendFile(path + 'index.html');
 });
 
 app.listen(PORT, HOST);
