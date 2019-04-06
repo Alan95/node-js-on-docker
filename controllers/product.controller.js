93aca43f6cfba2d01d524dac624639056bbad763
Alan Saber <battlecraft95@gmail.com>
2019-04-07 01:49:39 +0200

    Add first create route and dotenv and connect to mongodb atlas

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,3 +3,19 @@ const Product = require('../models/product.model');
 exports.test = function (req, res) {
     res.send('Test');
 }
+
+exports.product_create = function (req, res, next) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    )
+
+    product.save(function (err) {
+        if (err) {
+            next(err);
+        }
+        res.send('201');
+    })
+};
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 'use strict';
 
+const PORT = 8081;
+const HOST = '0.0.0.0';
+const path = __dirname + '/views/';
+const dotenv = require('dotenv');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = 8080;
-const HOST = '0.0.0.0';
-const path = __dirname + '/views/';
-
-const product = require('./routes/product.route');
+const product = require('./routes/product.route'); // Import routes
+dotenv.config();
 const app = express();
 
+//DB SETUP
+const mongoose = require('mongoose');
+const mongoDB = process.env.MONGODB_URI;
+
+mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
 app.listen(PORT, HOST);
