d9ffc98c5595a205f357cba0121176ba7040080a
Alan Saber <battlecraft95@gmail.com>
2019-04-09 00:07:46 +0200

    Add show product route and method

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,8 +1,5 @@
 const Product = require('../models/product.model');
-
-exports.test = function (req, res) {
-    res.send('Test');
-}
+const ObjectId = require('mongodb').ObjectID;
 
 exports.product_create = function (req, res, next) {
     let product = new Product(
@@ -19,3 +16,12 @@ exports.product_create = function (req, res, next) {
         res.send('201');
     })
 };
+
+exports.product_show = function (req, res, next) {
+    const product = Product.findOne({ _id: ObjectId(req.params.id)}, function (err, result) {
+        if (err) {
+            res.status(400).send({'error': err});
+        } 
+        res.status(200).send(result);
+    });
+}
